988566f20037b19dd63a77b91f2a8881b1337f40
Stockman-Jr <stockmanmimmi@gmail.com>
2022-12-07 08:42:33 +0000

    Add database models

--- drawing_challenge/models.py
+++ drawing_challenge/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Challenges(models.Model):
+    title = models.CharField(max_length=200)
+    challenge_prompt = models.CharField(max_length=500, unique=True)
+    featured_image = CloudinaryField('image', default='placeholder')
+    slug = models.SlugField(max_length=200, unique=True)
+    date_created = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-date_created']
+
+    def __str__(self):
+        return self.title
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    caption = models.CharField(max_length=1000, blank=True)
+    date_posted = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User, related_name='post_likes', blank=True)
+
+    class Meta:
+        ordering = ['-date_posted']
+
+    def __str__(self):
+        return self.caption
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+
+# class PostImage(models.Model):
+#    post = models.ForeignKey(Post, related_name='images', on_delete=models.CASCADE)
+#    image = CloudinaryField('image', default='placeholder')
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    content = models.CharField(max_length=800, blank=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-date_created']
+
+    def __str__(self):
+        return self.content
+
